d3ccaef733d66c631b8ebe4195c147a6f2a8699c
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2025-07-28 23:50:04 +0530

    Add files via upload

---
+++ DeleteTheMiddleNodeOfLL.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class DeleteTheMiddleNodeOfLL{
+    public static void main(String args[]){
+        //User Input 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements to insert : ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " elements : ");
+        Node head = null, tail = null;
+        for(int i = 0 ; i < n ; i++){
+            int value = sc.nextInt();
+            Node newnode = new Node(value);
+            if(head == null){
+                head = tail = newnode;
+            }
+            else{
+                tail.next = newnode;
+                tail = tail.next;
+            }
+        }
+        
+        System.out.println("The Updated LinkedList is : " + deleteMiddle(head));
+        printLL(head);
+    }
+    public static  Node deleteMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;    //To Keep Track ofslow node 
+        if(head == null || head.next == null){
+            return null;
+        }
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+        return head;
+    }
+    public static void printLL(Node head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+    Node(int data1, Node next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+    }
+}
+
